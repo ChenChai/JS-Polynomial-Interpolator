3ce2d380afa3c4eb5dba14806527691126e34043
ChenChai <chen.chai@outlook.com>
2019-01-19 14:46:12 -0500

    Boxes now turn red when value invalid.

--- interpolator.js
+++ interpolator.js
@@ -21,6 +21,28 @@ function addInputPair() {
     pointList.append(point);
 }
 
+// returns an array of points which have been input.
+function getArray() {
+    var pointForm = document.getElementById("pointList");
+
+    var numPoints = pointForm.childElementCount;
+    window.alert(numPoints);
+    return;
+
+    if (pointForm.childElementCount <= 1) {
+        return undefined;
+    }
+
+    var rawPoints = pointForm.childNodes;
+    var length = rawPoints.length;
+    
+    var vandermonde = [];
+    for(var i = 0; i < length; i++) {
+
+    }
+
+}
+
 // validates that inputs are good.
 function validateInputs() {
     var pointForm = document.getElementById("pointList");
@@ -32,23 +54,30 @@ function validateInputs() {
 
     // loop through each coordinate pair and validate
     for (var i = 0; i < numPoints; i++) {
+        // get a point
         var point = points[i];
         console.log("number of coords: " + point.childElementCount);
+
+        // get x and y values.
         var x = point.children[0];
         var y = point.children[1];
 
-        if (isNaN(x.nodeValue)) {
+        // check if x is valid
+        if (isNaN(x.value) || x.value == "") {
             valid = false;
             x.setAttribute("class", "invalid");
         } else {
             console.log("x is " + x.value);
+            x.setAttribute("class", "");
         }
 
-        if (isNaN(y.nodeValue)) {
+        // check if y is valid
+        if (isNaN(y.value) || y.value == "") {
             valid = false;
-            x.setAttribute("class", "invalid");
+            y.setAttribute("class", "invalid");
         } else {
             console.log("y is " + y.value);
+            y.setAttribute("class", "");
         }
     }
 
