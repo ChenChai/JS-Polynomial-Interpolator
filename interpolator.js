8acc07344ab1bc5b0162abb903aca813ec302236
ChenChai <chen.chai@outlook.com>
2019-01-19 14:40:02 -0500

    Added function to validate inputs

--- interpolator.js
+++ interpolator.js
@@ -3,7 +3,7 @@ function addInputPair() {
     var point = document.createElement("div");
 
     point.setAttribute("id", "point");
-    point.append("(");
+    //point.append("(");
     
     // create an input field
     var coord = document.createElement("input");
@@ -12,11 +12,52 @@ function addInputPair() {
     coord2.setAttribute("type", "text");    
 
     point.append(coord);
-    point.append(", ")
+    //point.append(", ")
     point.append(coord2);
 
-    point.append(")");
+    //point.append(")");
 
     var pointList = document.getElementById("pointList");
     pointList.append(point);
 }
+
+// validates that inputs are good.
+function validateInputs() {
+    var pointForm = document.getElementById("pointList");
+    var numPoints = pointForm.childElementCount;
+    console.log("Number of points: " + numPoints);
+    var points = pointForm.children;
+
+    var valid = true;
+
+    // loop through each coordinate pair and validate
+    for (var i = 0; i < numPoints; i++) {
+        var point = points[i];
+        console.log("number of coords: " + point.childElementCount);
+        var x = point.children[0];
+        var y = point.children[1];
+
+        if (isNaN(x.nodeValue)) {
+            valid = false;
+            x.setAttribute("class", "invalid");
+        } else {
+            console.log("x is " + x.value);
+        }
+
+        if (isNaN(y.nodeValue)) {
+            valid = false;
+            x.setAttribute("class", "invalid");
+        } else {
+            console.log("y is " + y.value);
+        }
+    }
+
+    return valid;
+}
+
+function submitPoints() {
+    var valid = validateInputs();
+
+    if (!valid) { return; }
+}
+
